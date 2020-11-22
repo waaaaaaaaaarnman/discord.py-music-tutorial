8aeafdfe170df08cb461d33826a6157da108a6d3
Kaede_728 <65491942+waaaaaaaaaarnman@users.noreply.github.com>
2020-11-22 12:07:53 +0900

    Update music.py

--- bot/cogs/music.py
+++ bot/cogs/music.py
@@ -19,37 +19,6 @@ OPTIONS = {
 }
 
 
-class AlreadyConnectedToChannel(commands.CommandError):
-    pass
-
-
-class NoVoiceChannel(commands.CommandError):
-    pass
-
-
-class QueueIsEmpty(commands.CommandError):
-    pass
-
-
-class NoTracksFound(commands.CommandError):
-    pass
-
-
-class PlayerIsAlreadyPaused(commands.CommandError):
-    pass
-
-
-class NoMoreTracks(commands.CommandError):
-    pass
-
-
-class NoPreviousTracks(commands.CommandError):
-    pass
-
-
-class InvalidRepeatMode(commands.CommandError):
-    pass
-
 
 class RepeatMode(Enum):
     NONE = 0
